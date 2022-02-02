a696ff24796ac347e17afa79f975844ad2634829
Justice Velasco <justicevgomez001@gmail.com>
2022-02-02 08:59:47 +0100

    🎇 first commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import AntDesign from "react-native-vector-icons/AntDesign"
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text>Helloooo</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
